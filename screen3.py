fc0142d13e616d279994cb8218c50dc89629f43e
이름 <  >
2024-05-16 22:19:00 +0900

    3가지 py파일로 각자 관리하는 파이프라인

---
+++ screen3.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+def switch_to_screen_3(window, reset_to_start_screen):
+    clear_window(window)
+    label = Label(window, text="세 번째 화면", font=("Helvetica", 20))
+    label.pack(pady=20)
+
+    # 여기에 세 번째 화면의 다른 위젯을 추가하세요.
+    back_button = Button(window, text="뒤로가기", command=lambda: reset_to_start_screen(window))
+    back_button.pack()
+
+
+def clear_window(window):
+    for widget in window.winfo_children():
+        widget.destroy()
--- start_screen.py
+++ start_screen.py
@@ -1,35 +1,8 @@
 from tkinter import *
 from tkinter import font
-
-
-def switch_to_screen_1(window):
-    clear_window(window)
-    label = Label(window, text="첫 번째 화면", font=("Helvetica", 20))
-    label.pack(pady=20)
-
-    # 여기에 첫 번째 화면의 다른 위젯을 추가하세요.
-    back_button = Button(window, text="뒤로가기", command=lambda: reset_to_start_screen(window))
-    back_button.pack()
-
-
-def switch_to_screen_2(window):
-    clear_window(window)
-    label = Label(window, text="두 번째 화면", font=("Helvetica", 20))
-    label.pack(pady=20)
-
-    # 여기에 두 번째 화면의 다른 위젯을 추가하세요.
-    back_button = Button(window, text="뒤로가기", command=lambda: reset_to_start_screen(window))
-    back_button.pack()
-
-
-def switch_to_screen_3(window):
-    clear_window(window)
-    label = Label(window, text="세 번째 화면", font=("Helvetica", 20))
-    label.pack(pady=20)
-
-    # 여기에 세 번째 화면의 다른 위젯을 추가하세요.
-    back_button = Button(window, text="뒤로가기", command=lambda: reset_to_start_screen(window))
-    back_button.pack()
+from screen1 import switch_to_screen_1
+from screen2 import switch_to_screen_2
+from screen3 import switch_to_screen_3
 
 
 def create_start_screen(window):
@@ -40,13 +13,16 @@ def create_start_screen(window):
     label = Label(window, text="MEF", font=OpenTopic)
     label.place(x=100, y=100)
 
-    button1 = Button(window, text="버튼 1", font=MainTopic, command=lambda: switch_to_screen_1(window))
+    button1 = Button(window, text="버튼 1", font=MainTopic,
+                     command=lambda: switch_to_screen_1(window, reset_to_start_screen))
     button1.place(x=100, y=300)
 
-    button2 = Button(window, text="버튼 2", font=MainTopic, command=lambda: switch_to_screen_2(window))
+    button2 = Button(window, text="버튼 2", font=MainTopic,
+                     command=lambda: switch_to_screen_2(window, reset_to_start_screen))
     button2.place(x=100, y=400)
 
-    button3 = Button(window, text="버튼 3", font=MainTopic, command=lambda: switch_to_screen_3(window))
+    button3 = Button(window, text="버튼 3", font=MainTopic,
+                     command=lambda: switch_to_screen_3(window, reset_to_start_screen))
     button3.place(x=100, y=500)
 
 
