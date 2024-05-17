dccf2f4b6fd786f118b54ae4d367d4fb8e250298
이름 <  >
2024-05-17 22:34:17 +0900

    open api에서 정보 들고 옴

--- assist.py
+++ assist.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import font
 import os
 
+import urllib.parse
+import http.client
+import json
+
 # 전역 변수로 이미지와 폰트를 로드
 background_image = None
 font_name = "충주김생체 TTF"
@@ -31,3 +35,29 @@ def clear_window(window):
 # 뒤로가기 버튼 생성 함수
 def create_back_button(window, reset_to_start_screen):
     return Button(window, text="뒤로가기",bg = '#efc376', command=lambda: reset_to_start_screen(window))
+
+# api 들고 오는 함수
+def fetch_data_from_api(host, endpoint, params):
+    # 파라미터를 인코딩합니다.
+    query_string = urllib.parse.urlencode(params)
+
+    # 연결을 만듭니다.
+    conn = http.client.HTTPConnection(host)
+
+    # 요청을 보냅니다.
+    conn.request("GET", f"{endpoint}?{query_string}")
+
+    # 응답을 받습니다.
+    response = conn.getresponse()
+
+    # 상태와 이유를 출력합니다.
+    print(response.status, response.reason)
+
+    # 응답 본문을 읽고 디코딩합니다.
+    data = response.read().decode('utf-8')
+
+    # 연결을 닫습니다.
+    conn.close()
+
+    # JSON 데이터로 변환하여 반환합니다.
+    return json.loads(data)
--- screen1.py
+++ screen1.py
@@ -8,6 +8,18 @@ def switch_to_screen_1(window, reset_to_start_screen):
     # 배경화면 설정
     set_background(window)
 
+    host = "apis.data.go.kr"
+    endpoint = "/1051000/public_inst/list"
+    params = {
+        "serviceKey": "GuwRZzKrYZA0iHG1Y%2BArdizUhu0a32Kym5AKO4tlpC71aaaCEI6YOzWIEfHyipefqThokj%2F9YurMG0WibwIfrA%3D%3D",
+        "pageNo": "1",
+        "numOfRows": "10",
+        "type": "json",
+    }
+    data = fetch_data_from_api(host, endpoint, params)
+
+    print(type(data))
+
     label_topic = Label(window, text="공공기관 시설정보", font=(font_name, 30), bg='#efc376')
     label_topic.place(x=50, y=50)
 
