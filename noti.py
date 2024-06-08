28195c5f671b2e0f5bbd00dbb48882a6f13e2ded
이름 <gnffk48@naver.com>
2024-06-08 21:31:59 +0900

    챗봇 완성

--- noti.py
+++ noti.py
@@ -2,15 +2,10 @@
 # coding=utf-8
 
 import sys
-import time
-import sqlite3
-import telepot
-from pprint import pprint
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import re
-from datetime import date, datetime, timedelta
 import traceback
+from pprint import pprint
+
+import telepot
 from assist import fetch_data_from_api
 
 TOKEN = '7435221852:AAFp7IvK36o1albxCVj8GP9DT1MUyCXNhi8'
@@ -65,6 +60,106 @@ def getData1(inst_type):
     # 결과 리스트 출력
     print("Result list:")
     print(res_list)
+
+    return res_list
+
+def getData2(inst_type):
+    global data, filtered_data, bizClsf
+
+    bizClsf = ""
+    if inst_type == '건강':
+        bizClsf = "B01"
+    elif inst_type == '공공안전':
+        bizClsf = 'B02'
+    elif inst_type == '교육연구':
+        bizClsf = 'B03'
+    elif inst_type == '국가인프라':
+        bizClsf = 'B04'
+    elif inst_type == '생활환경':
+        bizClsf = 'B05'
+    elif inst_type == '취업직업':
+        bizClsf = 'B06'
+    elif inst_type == '해외남북교류':
+        bizClsf = 'B07'
+    elif inst_type == '기타':
+        bizClsf = 'B08'
+    elif inst_type == '분류안됨':
+        bizClsf = 'NULL'
+
+    print(bizClsf)
+
+    data = []
+    pageNo = 1
+    while True:
+        host = "apis.data.go.kr"
+        endpoint = "/1051000/biz/list"
+        params = {
+            "serviceKey": "GuwRZzKrYZA0iHG1Y+ArdizUhu0a32Kym5AKO4tlpC71aaaCEI6YOzWIEfHyipefqThokj/9YurMG0WibwIfrA==",
+            "numOfRows": "1000",
+            "pageNo": pageNo
+        }
+        if bizClsf:
+            params["bizClsf"] = bizClsf
+        print(bizClsf)
+        response = fetch_data_from_api(host, endpoint, params)
+        if response and "result" in response:
+            page_data = response["result"]
+            if not page_data:
+                break
+            data.extend(page_data)
+            pageNo += 1
+        else:
+            break
+
+    if data:
+        filtered_data = data  # 초기 필터링 데이터 설정
+
+    else:
+        data = None
+        filtered_data = None
+        print("데이터를 불러올 수 없습니다.")
+
+    return filtered_data
+
+def getData3(inst_type):
+    global data, filtered_data
+    res_list = []
+    host = "apis.data.go.kr"
+    endpoint = "/1051000/recruitment/list"
+    params = {
+        "serviceKey": "GuwRZzKrYZA0iHG1Y+ArdizUhu0a32Kym5AKO4tlpC71aaaCEI6YOzWIEfHyipefqThokj/9YurMG0WibwIfrA==",
+        "numOfRows": "500"
+    }
+    response = fetch_data_from_api(host, endpoint, params)
+    if response and "result" in response:
+        data = response["result"]
+
+        filtered_data = data
+        print("API에서 불러온 데이터:")
+        print(f"불러온 데이터 개수: {len(filtered_data)}")
+    else:
+        data = None
+        filtered_data = None
+        print("데이터를 불러올 수 없습니다.")
+
+    for key in filtered_data:
+        instTypeNm = key.get('hireTypeNmLst')
+        if inst_type == instTypeNm:
+            instName = key.get('instNm')
+            instDesc = key.get('recrutPbancTtl')
+            row = f'{instName}: {instDesc}'
+            print(row)
+            res_list.append(row.strip())
+
+        elif instTypeNm == '청년인턴':
+            if instTypeNm == '청년인턴(체험형)' or instTypeNm == '청년인턴(채용형)':
+                instName = key.get('instNm')
+                instDesc = key.get('recrutPbancTtl')
+                row = f'{instName}: {instDesc}'
+                print(row)
+                res_list.append(row.strip())
+
+
 
     return res_list
 
--- teller.py
+++ teller.py
@@ -30,6 +30,29 @@ def send_company_categories(chat_id):
                           "3. 기타공공기관")
     noti.sendMessage(chat_id, categories_message)
 
+def send_busy_categories(chat_id):
+    categories_message = ("원하시는 사업 정보를 선택해주세요\n"
+                          "1. 건강\n"
+                          "2. 공공안전\n"
+                          "3. 교육연구\n"
+                          "4. 국가인프라\n"
+                          "5. 문화생활\n"
+                          "6. 사회복지\n"
+                          "7. 산업진흥\n"
+                          "8. 생활환경\n"
+                          "9. 취업직업\n"
+                          "10. 해외남북교류\n"
+                          "11. 기타\n"
+                          "12. 분류안됨")
+    noti.sendMessage(chat_id, categories_message)
+
+def send_categories(chat_id):
+    categories_message = ("원하시는 기업 정보를 선택해주세요\n"
+                          "1. 정규직\n"
+                          "2. 무기계약직\n"
+                          "3. 비정규직\n"
+                          "4. 청년인턴\n")
+    noti.sendMessage(chat_id, categories_message)
 
 def handle_company_details(chat_id, category):
     inst_type_map = {
@@ -50,6 +73,30 @@ def handle_company_details(chat_id, category):
     noti.sendMessage(chat_id, details_message)
     send_welcome_message(chat_id)  # 정보를 출력한 후 다시 초기 화면으로 돌아갑니다.
 
+def handle_busy_details(chat_id, category):
+    res_list = noti.getData2(category)
+    if res_list:
+        details_message = '\n'.join([f"{item['bizNm']}" for item in res_list])
+    else:
+        details_message = f'{category}에 대한 정보를 찾을 수 없습니다.'
+
+    noti.sendMessage(chat_id, details_message)
+    send_welcome_message(chat_id)  # 정보를 출력한 후 다시 초기 화면으로 돌아갑니다.
+
+def handle_details(chat_id, inst_type):
+
+    if inst_type:
+        res_list = noti.getData3(inst_type)
+
+        if res_list:
+            details_message = '\n'.join(res_list)
+        else:
+            details_message = f'{inst_type}에 대한 정보를 찾을 수 없습니다.'
+    else:
+        details_message = "잘못된 선택입니다. 다시 시도해주세요."
+
+    noti.sendMessage(chat_id, details_message)
+    send_welcome_message(chat_id)  # 정보를 출력한 후 다시 초기 화면으로 돌아갑니다.
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     if content_type != 'text':
@@ -65,10 +112,14 @@ def handle(msg):
         send_company_categories(chat_id)
     elif text in ['공기업', '준정부기관', '기타공공기관']:
         handle_company_details(chat_id, text)
-    elif text.startswith('채용'):
-        noti.sendMessage(chat_id, '채용 정보를 출력합니다...')
     elif text.startswith('사업'):
-        noti.sendMessage(chat_id, '사업 정보를 출력합니다...')
+        send_busy_categories(chat_id)
+    elif text in ['건강', '공공안전', '교육연구', '국가인프라', '문화생활', '사회복지', '산업진흥','생활환경','취업직업','해외남북교류','기타','분류안됨']:
+        handle_busy_details(chat_id, text)
+    elif text.startswith('채용'):
+        send_categories(chat_id)
+    elif text in ['정규직', '무기계약직', '비정규직', '청년인턴']:
+        handle_details(chat_id, text)
     else:
         noti.sendMessage(chat_id, '모르는 명령어입니다.\n')
 
@@ -80,5 +131,5 @@ def start_telegram_bot():
         time.sleep(10)
 
 # 이 부분은 메인 프로그램에서 직접 실행하지 않도록 주석 처리합니다.
-# if __name__ == '__main__':
+# if __name__ == '__main__'
 #    start_telegram_bot()
